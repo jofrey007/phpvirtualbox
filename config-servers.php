91bc0ec6209eba5590c646750f5e39f56e063d18
Attila Csontos <attila.csontos@r-sys.sk>
2024-11-04 13:26:49 +0000

    initial commit

---
+++ config-servers.php
@@ -0,0 +1,51 @@
+<?php return array (
+  0 => 
+  array (
+    'name' => 'Vboxserver',
+    'username' => 'rsys',
+    'password' => '"Rysys1029384-007!"',
+    'location' => 'http://10.30.0.30:5678/',
+  ),
+  1 => 
+  array (
+    'name' => 'DockerServer',
+    'username' => 'rsys',
+    'password' => '"Rysys1029384-007!"',
+    'location' => 'http://10.30.0.50:5678/',
+  ),
+  2 => 
+  array (
+    'name' => 'Ixotest',
+    'username' => 'rsys',
+    'password' => '"Rsys1029384-007!"',
+    'location' => 'http://176.102.98.6:5678/',
+  ),
+  3 => 
+  array (
+    'name' => 'Uxleaks',
+    'username' => 'rsys',
+    'password' => '"Rsys1029384-007!"',
+    'location' => 'http://176.102.98.7:5678/',
+  ),
+  4 => 
+  array (
+    'name' => 'Ixotest-backup',
+    'username' => 'rsys',
+    'password' => '"Rsys1029384-007!"',
+    'location' => 'http://10.31.0.239:5678/',
+  ),
+  5 => 
+  array (
+    'name' => 'Server01ng',
+    'username' => 'rsys',
+    'password' => '"Rsys1029384-007!"',
+    'location' => 'http://10.30.0.10:5678/',
+  ),
+  6 => 
+  array (
+    'name' => 'Serverzv',
+    'username' => 'eris',
+    'password' => '"eris"',
+    'location' => 'http://10.33.0.120:5678/',
+  ),
+);
